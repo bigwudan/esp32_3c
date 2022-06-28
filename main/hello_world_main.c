44242ebe46f4a690f2ed66b26f9b28520b892cec
bigwudan <bigwudan@qq.com>
2022-06-28 09:31:12 +0800

    增加旋钮

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -42,6 +42,74 @@
 
 
 
+/* GPIO Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+
+
+#define GPIO_INPUT_IO_0     0
+#define GPIO_INPUT_IO_1     1
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+
+
+static void _set_knob(){
+    gpio_config_t io_conf = {};
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //change gpio intrrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    //xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
+
+    //remove isr handler for gpio number.
+    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin again
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+
+
+}
 
 
 
@@ -96,11 +164,32 @@ void _my_task(){
 
 }
 
+
+static void _run_bg(){
+
+
+    gpio_pad_select_gpio(3);                 // 选择GPIO口
+    gpio_set_direction(3, GPIO_MODE_OUTPUT); // GPIO作为输出
+    gpio_set_level(3, 0);                    // 默认低电平
+
+
+
+}
+
+
+
+
+
 void app_main(void)
 {
     printf("***********app_main now.\n");
     printf("***********app_main now.\n");
     printf("***********app_main now.\n");
+
+
+
+    uint32_t io_num;
+    _set_knob();
     bluetooth_app();
     fast_scan();
     lcd_dev_init();
@@ -122,7 +211,10 @@ void app_main(void)
 
     //lv_task_create(_my_task, 500, LV_TASK_PRIO_MID, NULL);
     while(1){
-	    vTaskDelay(2000 / portTICK_PERIOD_MS);
+	    vTaskDelay(20 / portTICK_PERIOD_MS);
+        if(xQueueReceive(gpio_evt_queue, &io_num, 0)) {
+            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        } 
         lv_task_handler();
         //_test_task();
         _my_task();
@@ -132,3 +224,4 @@ void app_main(void)
 
 
 }
+#endif
