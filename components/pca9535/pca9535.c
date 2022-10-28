b4e1d181057e6f27127a29dbc2bcd60c89096b43
bigwudan <bigwudan@qq.com>
2022-10-28 09:26:22 +0800

    修改之前的代码

--- components/pca9535/pca9535.c
+++ components/pca9535/pca9535.c
@@ -13,6 +13,41 @@ static const char *TAG = "PCA9535";
 ExtIO_Reg_Type extIO_OutReg[2] = {0x00}; // 2组io
 ExtIO_Reg_Type extIO_InReg[2] = {0x00};  // 2组io
 
+
+
+
+//static const char *TAG = "i2c-simple-example";
+
+#define I2C_MASTER_SCL_IO 2         /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO 1         /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM 0            /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ 100000   /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS 1000
+
+/**
+ * @brief i2c master initialization
+ */
+static esp_err_t
+i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+
+    i2c_param_config(i2c_master_port, &conf);
+
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
 static void extio_outreg_write_bit(uint8_t regNum, uint8_t bitNum, uint8_t value)
 {
     if (value)
@@ -32,7 +67,12 @@ void pca9535_init(void)
     // IO_1 = 0000 0000  0x00
     static const uint8_t pca9535_setbuff[3] = {0x06, 0x1F, 0x00};
 
+    i2c_master_init();
+    
     ESP_LOGI(TAG, "设置输入输出");
+
+
+
     ESP_ERROR_CHECK(i2c_master_write_to_device(I2C_MASTER_NUM, PCA9535_ADDR, pca9535_setbuff, sizeof(pca9535_setbuff), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS));
 }
 
--- main/app_main.c
+++ main/app_main.c
@@ -34,45 +34,12 @@
 #include "pca9535.h"
 #include "pcf8563.h"
 
-
 static const char TAG[] = "app_main";
 
-//static const char *TAG = "i2c-simple-example";
-
-#define I2C_MASTER_SCL_IO 2         /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO 1         /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM 0            /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
-#define I2C_MASTER_FREQ_HZ 100000   /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_TIMEOUT_MS 1000
-
-/**
- * @brief i2c master initialization
- */
-static esp_err_t
-i2c_master_init(void)
-{
-    int i2c_master_port = I2C_MASTER_NUM;
-
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_MASTER_SDA_IO,
-        .scl_io_num = I2C_MASTER_SCL_IO,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_MASTER_FREQ_HZ,
-    };
-
-    i2c_param_config(i2c_master_port, &conf);
-
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-}
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK(i2c_master_init());
-    ESP_LOGI(TAG, "I2C initialized successfully");
+
 
     pca9535_init();
     ESP_LOGI(TAG, "pca9535 initialized successfully");
@@ -98,8 +65,7 @@ void app_main(void)
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 
-    ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
-    ESP_LOGI(TAG, "I2C de-initialized successfully");
+
 }
 
 
